f812269ca12dcb8df30649a8700acd9f517fccb2
Dave Knight <d7knight@uwaterloo.ca>
2015-05-20 16:17:30 -0400

    changed server info

--- app/src/main/java/com/example/cs446project/bestfuel/app/AppConfig.java
+++ app/src/main/java/com/example/cs446project/bestfuel/app/AppConfig.java
@@ -2,8 +2,8 @@ package com.example.cs446project.bestfuel.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://192.168.0.104/android_login_api/";
+	public static String URL_LOGIN = "http://cs446proj.atwebpages.com/android_login_api/";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://192.168.0.104/android_login_api/";
+	public static String URL_REGISTER = "http://cs446proj.atwebpages.com/android_login_api/";
 }
