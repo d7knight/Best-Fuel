4a1f17f5fb30f193591c2d89fc9c54d1ef2b1562
Dave Knight <d7knight@uwaterloo.ca>
2015-05-20 16:04:54 -0400

    Added Login and Registration Activities

---
+++ app/src/main/java/com/example/cs446project/bestfuel/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.cs446project.bestfuel.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.0.104/android_login_api/";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://192.168.0.104/android_login_api/";
+}
