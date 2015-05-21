48308e6c1ea1d84019b8f00633c54abb2c217d27
Dave Knight <d7knight@uwaterloo.ca>
2015-05-21 12:42:44 -0400

    fixed up the javascript interface added a html file to the assets folder for displaying google maps

--- app/src/main/java/com/example/cs446project/bestfuel/MapActivity.java
+++ app/src/main/java/com/example/cs446project/bestfuel/MapActivity.java
@@ -1,22 +1,119 @@
 package com.example.cs446project.bestfuel;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 
 public class MapActivity extends Activity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_map);
-        WebView webview=(WebView)findViewById(R.id.webView);
-        webview.loadUrl("file:///android_asset/test.html");
+        WebView webview=(WebView)findViewById(R.id.webkit);
+        webview.getSettings().setJavaScriptEnabled(true);
+        //Inject WebAppInterface methods into Web page by having Interface name 'Android'
+        webview.addJavascriptInterface(new WebAppInterface(this), "Android");
+        //Load URL inside WebView
+
+        webview.loadUrl("file:///android_asset/map.html");
+    }
+
+
+    //Class to be injected in Web page
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /**
+         * Show Toast Message
+         * @param toast
+         */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+
+        /**
+         * Show Dialog
+         * @param dialogMsg
+         */
+        @JavascriptInterface
+        public void showDialog(String dialogMsg){
+            AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+
+            // Setting Dialog Title
+            alertDialog.setTitle("JS triggered Dialog");
+
+            // Setting Dialog Message
+            alertDialog.setMessage(dialogMsg);
+
+            // Setting alert dialog icon
+            //alertDialog.setIcon((status) ? R.drawable.success : R.drawable.fail);
+
+            // Setting OK Button
+            alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(mContext, "Dialog dismissed!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            // Showing Alert Message
+            alertDialog.show();
+        }
+
+        /**
+         * Intent - Move to next screen
+         */
+        @JavascriptInterface
+        public void moveToNextScreen(){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+            // Setting Dialog Title
+            alertDialog.setTitle("Alert");
+            // Setting Dialog Message
+            alertDialog.setMessage("Are you sure you want to leave to next screen?");
+            // Setting Positive "Yes" Button
+            alertDialog.setPositiveButton("YES",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Move to Next screen
+                       //     Intent chnIntent = new Intent(MapActivity.this, ChennaiIntent.class);
+                       //     startActivity(chnIntent);
+                        }
+                    });
+            // Setting Negative "NO" Button
+            alertDialog.setNegativeButton("NO",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Cancel Dialog
+                            dialog.cancel();
+                        }
+                    });
+            // Showing Alert Message
+            alertDialog.show();
+        }
     }
 
 
