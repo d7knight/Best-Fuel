954a9bc166c1979c2d474d7d32425dac74c089f5
Kyle Platt <platky1@gmail.com>
2015-05-27 15:23:11 -0400

    CarQuery API Complete

--- app/src/main/java/com/example/cs446project/bestfuel/ProfileActivity.java
+++ app/src/main/java/com/example/cs446project/bestfuel/ProfileActivity.java
@@ -59,30 +59,77 @@ public class ProfileActivity extends Activity {
             getMakes(2009);
         }
 
+
+        @JavascriptInterface
+        public void initd(String str) {
+            Log.d("CQ", "done initializing " + str);
+        }
+
+        //returns the range of all possible years for vehicles
         @JavascriptInterface
         public void getYears(){
             webview.loadUrl("javascript:getYears()");
         }
 
+        //RETURNS: JSON String {"min_year":"#", "max_year":"#"}
         @JavascriptInterface
         public void getYearsRet(String retObj) {
+
             Log.d("CQ", "the actual retobj is " + retObj);
         }
 
-        @JavascriptInterface
-        public void initd(String str) {
-            Log.d("CQ", "done initializing "+ str);
-        }
-
+        //returns all the possible makes in a specified year
         @JavascriptInterface
         public void getMakes(int year){
             webview.loadUrl("javascript:getMakes(" + year + ")");
         }
 
+        //RETURNS: JSON String [{"make_id":"ac","make_display":"AC","make_is_common":"0","make_country":"UK"},
+        //{"make_id":"acura","make_display":"Acura","make_is_common":"1","make_country":"USA"}...]
         @JavascriptInterface
         public void getMakesRet(String retObj) {
             Log.d("CQ", "the actual retobj for makes is " + retObj);
         }
+
+        //gets all the models from a specified make in a specified year
+        //make example = "ford"
+        @JavascriptInterface
+        public void getModels(String make, int year) {
+            webview.loadUrl("javascript:getModels(" + make + "," + year + ")");
+        }
+
+        //RETURNS: JSON String [{"model_name":"Escape","model_make_id":"ford"}...]
+        @JavascriptInterface
+        public void getModelsRet(String retObj) {
+            Log.d("CQ", "the actual retobj for models is " + retObj);
+        }
+
+        //gets all the details you need including vehicle id. Necessary step as there can be
+        //many versions of the same/similar vehicle
+        //PARAMETERS: fullDetails is 1 for all data or 0 for sparse data, test both to see what you need
+        @JavascriptInterface
+        public void getData(String make, String model, int year, int fullDetails) {
+            webview.loadUrl("javascript:getData(" + make + "," + year + "," + model + "," + fullDetails + ")");
+        }
+
+        //returns a ton of data... print out to test it
+        @JavascriptInterface
+        public void getDataRet(String retObj) {
+            Log.d("CQ", "the actual retobj for data is " + retObj);
+        }
+
+        //you need to grab vehicleId from getData and run it through here to get the exact car
+        @JavascriptInterface
+        public void getVehicle(int vehicleID) {
+            webview.loadUrl("javascript:getVehicle(" + vehicleID + ")");
+        }
+
+        //returns all available details about the car. print out to test
+        @JavascriptInterface
+        public void getVehicleRet(String retObj) {
+            Log.d("CQ", "the actual retobj for vehicle is " + retObj);
+        }
+
 
     }
 
