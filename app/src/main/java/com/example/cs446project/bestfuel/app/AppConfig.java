275b187095be2ea50d0da2398bba8b9595d792d0
Dave Knight <d7knight@uwaterloo.ca>
2015-05-28 21:12:03 -0400

    Changed urls to new server

--- app/src/main/java/com/example/cs446project/bestfuel/app/AppConfig.java
+++ app/src/main/java/com/example/cs446project/bestfuel/app/AppConfig.java
@@ -2,8 +2,8 @@ package com.example.cs446project.bestfuel.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://cs446proj.atwebpages.com/android_login_api/";
+	public static String URL_LOGIN = "http://www.cs446project.5gb.co/android_login_api/";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://cs446proj.atwebpages.com/android_login_api/";
+	public static String URL_REGISTER = "http://www.cs446project.5gb.co/android_login_api/";
 }
